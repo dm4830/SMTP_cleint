d9bf83b2635f3bbf76444f47a44fee0a3e478433
whateverusername <dani@gmail.com>
2021-02-21 20:11:14 +0200

    first

--- solution.py
+++ solution.py
@@ -20,9 +20,9 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
    #print('220 reply not received from server.')
 
    # Send HELO command and print server response.
-   heloCommand = 'HELO Alice\r\n'
-   clientSocket.send(heloCommand.encode())
-   recv1 = clientSocket.recv(1024).decode()
+   #heloCommand = 'HELO Alice\r\n'
+   #clientSocket.send(heloCommand.encode())
+   #recv1 = clientSocket.recv(1024).decode()
    #print(recv1)
    #if recv1[:3] != '250':
    #print('250 reply not received from server.')
